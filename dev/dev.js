e68048906a71edd4409682847797ff60a97b81c4
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-23 09:02:41 -0600

    Updated boilerplate

--- dev/dev.js
+++ dev/dev.js
@@ -6,10 +6,14 @@ const containerEl = document.createElement('div')
 document.body.appendChild(containerEl)
 
 render(
-	<HubspotForm onSubmit={e => console.log('Success!')}>
+	<HubspotForm
+		onSubmit={e => console.log('Success!')}
+		hubspotId='4111488'
+		formId='d9099643-7ccf-4305-988d-cb9ff9c1468a'
+		>
 		<form>
 			<div>
-				<input type='text' name='test' />
+				<input type='text' name='Email' />
 			</div>
 			<div>
 				<button>Submit</button>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import fetch from 'isomorphic-fetch'
+import querystring from 'querystring'
+import cookies from 'js-cookie'
 
 export default class extends React.Component {
 	constructor(props){
@@ -7,25 +9,56 @@ export default class extends React.Component {
 		this.formSubmit = this.formSubmit.bind(this)
 	}
 	componentDidMount(){
-		const form = this.parent.querySelector('form')
-		if(form){
-			form.addEventListener('submit', this.formSubmit)
+		this.form = this.parent.querySelector('form')
+		if(this.form){
+			this.form.addEventListener('submit', this.formSubmit)
 		}
 	}
-	formSubmit(e){
+	async formSubmit(e){
 		e.preventDefault()
+
+		// Run onSubmit at the same time as Hubspot code
 		if(this.props.onSubmit){
 			this.props.onSubmit()
 		}
-		// Hubspot stuff here
 
-		let data = new FormData(e.target)
+		let hutk = cookies.get('hubspotutk')
+		if(!hutk){
+			return console.log('hubspotutk not found')
+		}
+
+		// Get form data
+		let data = new FormData(this.form)
 		let result = {}
 		for (let entry of data.entries()) {
 			result[entry[0]] = entry[1]
 		}
-		console.log(result)
+		let pageName = document.querySelector('title')
+		pageName = pageName ? pageName.textContent : ''
+		data = querystring.stringify({
+			hs_context: JSON.stringify({
+				hutk: hutk,
+				pageUrl: window.location.href,
+				pageName: pageName,
+			}),
+			...data,
+		})
+		try{
+			let res = await fetch(`https://forms.hubspot.com/uploads/form/v2/${this.props.hubspotId}/${this.props.formId}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Content-Length': data.length
+				},
+				body: data,
+			})
+			console.log('RES:', res)
+		}
+		catch(err){
+			console.log(err)
+		}
 
+		// Run onSuccess after Hubspot code
 		if(this.props.onSuccess){
 			this.props.onSuccess()
 		}
